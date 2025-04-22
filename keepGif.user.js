666c75fde9473aa1b56b7338a64e5219ebd5d403
Multarix <darien2105@gmail.com>
2025-04-22 16:48:20 +1000

    Made the CSS more clear

--- keepGif.user.js
+++ keepGif.user.js
@@ -6,17 +6,56 @@
 // @homepageURL https://github.com/Multarix/Discord-Remove-Useless-UI-Elements
 // @supportURL  https://github.com/Multarix/Discord-Remove-Useless-UI-Elements/issues
 // @grant       none
-// @version     1.7.0
+// @version     1.7.1
 // @author      Multarix
-// @description Working as of: 29/03/2025
+// @description Working as of: 22/04/2025
 // ==/UserScript==
+
+
+let baseCss = `
+/* Remove "Start activity button */
+[class^="channelAppLauncher"]{
+	display: none;
+}
+
+
+/* Unfuckifiy the user panel */
+.visual-refresh section[aria-label="User area"] {
+	width: calc(var(--custom-guild-sidebar-width) - var(--custom-guild-list-width));
+	left: calc(var(--custom-guild-list-width) + 1px);
+	bottom: 0px;
+	border-radius: 0px;
+	border: none;
+}
+.visual-refresh section[aria-label="User area"] [class*="activityPanel"] {
+	border-radius: 0px;
+}
+.visual-refresh nav:has([data-list-id="guildsnav"]) {
+	margin-bottom: unset;
+}
+
+
+/* Remove Nameplates */
+[style^="background: linear-gradient("]{
+	display:none;
+}
+[src*="nameplates"]{
+	display:none;
+}
+
+
+/* Remove Activity Status on servers that don't turn it off */
+[class*="membersGroup"]:has([role=button]),
+[class*="member"] [class*="container"]:has([class*="badges"]) {
+	display: none;
+}
+`;
+
 
 const buttonsToHide = ["Open sticker picker", "Send a gift", "Boost this server", "Apps"];
 let css = "";
 buttonsToHide.forEach(button => css = css.concat(`[aria-label="${button}"]{display:none}`));		// Remove chat bar buttons
-css = css.concat('div[class^="channelAppLauncher"]{display:none}');									// Remove "Start activity button"
-css = css.concat('div[class*="activityPanel"]{display:none}');										// Remove intrusive "game being played" bar
-css = css.concat('div[style*="background: linear-gradient"]{display:none}');						// Remove Nameplates
-css = css.concat('img[src*="nameplates"]{display:none}');
+css += baseCss;
+
 const style = document.createElement('style'); style.innerHTML = css;
 document.body.appendChild(style);
